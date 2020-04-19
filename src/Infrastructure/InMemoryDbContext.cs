8aee280cfea199c2fb1f1c6a13afde0cb4045e79
Felix Firsching <felix.firsching@gmail.com>
2020-04-19 19:20:25 +0200

    added missing SaveChangesAsync implementation

--- src/Infrastructure/InMemoryDbContext.cs
+++ src/Infrastructure/InMemoryDbContext.cs
@@ -3,19 +3,24 @@ using Microsoft.EntityFrameworkCore;
 using Application.Interfaces;
 using Domain.Entities;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Infrastructure
 {
     public class InMemoryDbContext : DbContext, IDbContext
     {
 
-  
+
         public InMemoryDbContext(DbContextOptions<InMemoryDbContext> options) : base(options)
         {
         }
 
         public DbSet<WeatherForecast> WeatherForecast { get; set; }
 
-      
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync(new CancellationToken());
+        }
     }
 }
